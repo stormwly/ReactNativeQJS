24f477e27c44058205bfc9f6d0cac5f99087a251
zhangshili <zhangshili@qianjins.com>
2017-11-20 13:24:56 +0800

    删除调试log

--- ReactNativeQJS/src/page/HomePage.js
+++ ReactNativeQJS/src/page/HomePage.js
@@ -18,21 +18,16 @@ class HomePage extends Component {
     }
 
     _onBackAndroid = () => {
-        console.log('HomePage11111---',this.props)
         let {routes} = this.props;
         var now = new Date().getTime();
         if (routes && routes.length === 1) {
             if (now - lastClickTime < 2500) {
-                console.log('HomePage222222---',this.props)
-
                 return false;
             } else {
-                console.log('HomePage33333--',this.props)
                 toastShort('再点击一次退出应用');
             }
         } else {
             if (routes.length > 1) {
-                console.log('HomePage44444--',this.props)
                 this.props.navigation.goBack();
                 return true;
             }
